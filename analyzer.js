8303983dbc40ac5543d33a9ec951ec55ebb246d6
rondlite <rondlite@gmail.com>
2024-10-19 02:55:35 +0000

    inter

--- analyzer.js
+++ analyzer.js
@@ -12,9 +12,34 @@ export class SemanticAnalyzer {
       case "Program":
         node.children.forEach(child => this._analyzeNode(child));
         break;
+      case "VariableDeclaration":
+        this._analyzeNode(node.value.expression);
+        break;
+      case "PrintStatement":
+        this._analyzeNode(node.value.expression);
+        break;
+      case "IfStatement":
+        this._analyzeNode(node.value.condition);
+        this._analyzeNode(node.value.thenBranch);
+        if (node.value.elseBranch) {
+          this._analyzeNode(node.value.elseBranch);
+        }
+        break;
       case "ComparisonExpression":
         this._analyzeNode(node.value.left);
         this._analyzeNode(node.value.right);
+        break;
+      case "ConstantDeclaration":
+        this._analyzeNode(node.value.expression);
+        break;
+      case "ForEachLoop":
+        this._analyzeNode(node.value.body);
+        break;
+      case "FunctionDeclaration":
+        this._analyzeNode(node.value.body);
+        break;
+      case "ConcurrencyBlock":
+        node.value.children.forEach(child => this._analyzeNode(child));
         break;
       // Add other cases for different node types
       default:
